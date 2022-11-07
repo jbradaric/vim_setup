5dcae89d8f50a6783be0c99ed59571925217e733
Jurica Bradaric <jurica.bradaric@avl.com>
2022-11-07 14:08:36 +0100

    Use vim.treesitter functions for indent

--- nvim/lua/config/indent.lua
+++ nvim/lua/config/indent.lua
@@ -1,31 +1,9 @@
-local ts_utils = require('nvim-treesitter.ts_utils')
-local parsers = require('nvim-treesitter.parsers')
+local ts = vim.treesitter
 
 local M = {}
 
-function M.get_node_at_pos(line, col)
-  local winnr = 0
-  local cursor = { line, col }
-  local cursor_range = { cursor[1] - 1, cursor[2] }
-
-  local buf = vim.api.nvim_win_get_buf(winnr)
-  local root_lang_tree = parsers.get_parser(buf)
-  if not root_lang_tree then
-    return
-  end
-
-  local root
-  root = ts_utils.get_root_for_position(cursor_range[1], cursor_range[2], root_lang_tree)
-
-  if not root then
-    return
-  end
-
-  return root:named_descendant_for_range(cursor_range[1], cursor_range[2], cursor_range[1], cursor_range[2])
-end
-
 function M.get_node_type_at_pos(line, col)
-  local node = M.get_node_at_pos(line, col)
+  local node = ts.get_node_at_pos(0, line - 1, col)
   if not node then
     return ''
   end
