bdb5e5925332858ac5cfa95b536d3836623c9773
Jurica Bradaric <jbradaric@gmail.com>
2023-07-07 22:18:41 +0200

    Use nicer dap icons

--- nvim/lua/config/dap.lua
+++ nvim/lua/config/dap.lua
@@ -91,6 +91,20 @@ M.setup = function()
 
   dapui.setup({
     layouts = dapui_layout,
+    icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+    controls = {
+      icons = {
+        pause = '⏸',
+        play = '▶',
+        step_into = '⏎',
+        step_over = '⏭',
+        step_out = '⏮',
+        step_back = 'b',
+        run_last = '▶▶',
+        terminate = '⏹',
+        disconnect = '⏏',
+      },
+    },
   })
   dap.listeners.after.event_initialized['dapui_config'] = function()
     setup_mappings()
